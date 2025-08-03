3a3c4067f8008e00faea45497055e8b9fbefa45c
pedropaulo <pedropaulodecarlo@gmail.com>
2025-08-02 22:25:02 -0300

    Implementa features faltantes:
    - Implementar as propriedades faltantes de acordo com o diagrama
    - Passar os parâmetros do construtor para as propriedades
    - Herdar da classe "Smartphone" (para as duas classes filhas)
    - Sobrescrever o método "InstalarAplicativo" (para as duas classes filhas)

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
+        // DONE: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero,  string modelo, string imei,  int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // DONE: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
+Nokia nokia = new Nokia("10", "NokiaPlus", "012345-67-890123-4", 10);
+Iphone iphone = new Iphone("90", "X", "012345-67-890123-5",  100);
+nokia.InstalarAplicativo("WhatsApp");
+iphone.InstalarAplicativo("Facebook");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
